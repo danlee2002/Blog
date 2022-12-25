cc27dcf47a8c99d057a0e6cd5af74ed30f10f5ab
danlee2002 <danlee@gatech.edu>
2022-12-25 14:35:16 -0500

    added blog db

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
-posts = [
-    {'author': 'Daniel Lee',
-    'title' :  'Blog post 1',
-    'content' : 'First post content',
-    'date_posted' : 'August 28, 2022'},
-     {'author': 'Daniel Lee',
-    'title' :  'Blog post 1',
-    'content' : 'First post content',
-    'date_posted' : 'December 25, 2022'}
-]
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 def about(request, ):
