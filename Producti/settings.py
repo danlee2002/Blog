ab23a75c7d5c9c90b91c945ca698e6894b56a597
danlee2002 <danlee@gatech.edu>
2022-12-26 21:25:05 -0500

    test

--- Producti/settings.py
+++ Producti/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
-    "users.app.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
